26da151edae9f9acfd215ae178d962aff430dba3
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2023-12-06 17:32:30 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import MyStack from './src/Navigation';
+import MyStack from './Navigation';
 
 const App = () => {
   return (
